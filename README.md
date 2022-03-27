fada61450e11e9706672c5994be31ebe24d68176
flux6663 <flux6593@gmail.com>
2022-03-27 20:46:37 +0200

    Création API pour Application

--- README.md
+++ README.md
@@ -8,9 +8,9 @@
 # Installation
 ## Récupéré le Programme
 
-Télécharger Via GitHub le programme [BotBar - V1.0.0](https://pages.github.com/).
+    Télécharger Via GitHub le programme [BotBar - V1.0.0](https://pages.github.com/).
 
-:warning: Attention : il faut avoire installer PlatformIO sur Visual Studio CODE !
+    :warning: Attention : il faut avoire installer PlatformIO sur Visual Studio CODE !
 
 ## Ajouter le programme a PlatformIO
 
@@ -18,23 +18,33 @@ Télécharger Via GitHub le programme [BotBar - V1.0.0](https://pages.github.com
 
 2. Ajouter dans la mémoire de l'ESP le code HTML de la page Web
 
-![This is an image](https://www.raspberryme.com/checklist/2022/01/1642112646_967_ESP8266-VS-Code-PlatformIO-telecharger-des-fichiers-sur-le-systeme.jpg)
+    ![This is an image](https://www.raspberryme.com/checklist/2022/01/1642112646_967_ESP8266-VS-Code-PlatformIO-telecharger-des-fichiers-sur-le-systeme.jpg)
 
 3. Integration de l'ESP au WiFi personnele
 
-[dans src/main.cpp](src/main.cpp) remplacer les valeur du ssid et du password
+    [dans src/main.cpp](src/main.cpp) remplacer les valeur du ssid et du password
 
-```c
-const char* ssid = "NOM RESEAU WIFI";          // <<--- METTRE ICI VOTRE NOM RESEAU WIFI
-const char* password = "MOT DE PASSE WIFI";    // <<--- METTRE ICI VOTRE MOT DE PASSE WIFI
-```
+    ```c
+    const char* ssid = "NOM RESEAU WIFI";          // <<--- METTRE ICI VOTRE NOM RESEAU WIFI
+    const char* password = "MOT DE PASSE WIFI";    // <<--- METTRE ICI VOTRE MOT DE PASSE WIFI
+    ```
 
-Remplacer les Valeur "NOM RESEAU WIFI" par le nom de votre WiFI priver.
-Remplacer les Valeur "MOT DE PASSE WIFI" par le MDP de votre WiFI priver.
+    Remplacer les Valeur "NOM RESEAU WIFI" par le nom de votre WiFI priver.
+    Remplacer les Valeur "MOT DE PASSE WIFI" par le MDP de votre WiFI priver.
 
 4. Téléverser le Programme avec la fleche en bas a gauche de l'écrant de VS Code.
 
 5. Se Connecter a la page Web
 
-En bas a gauche de l'écrant, se trouve une prise, elle permet d'ouvrire le terminale.
-Une foie le terminale ouvert, l'ip de l'ESP doit apparaître.
+    En bas a gauche de l'écrant, se trouve une prise, elle permet d'ouvrire le terminale.
+    Une foie le terminale ouvert, l'ip de l'ESP doit apparaître.
+
+## API
+
+    Liste des requette fonctionelle :
+
+    > /connexion
+    Retourne "connexion OK"
+
+    > /reqEtatBouton
+    Retourne "En cours" puis "Terminer" en fonction des etat d'avencement de la machine
--- src/main.cpp
+++ src/main.cpp
@@ -5,81 +5,35 @@
 #include <WiFiManager.h>
 #include <LittleFS.h>
 
-const char * nomDeFichier = "/Hello.html";  // <<--- NOM DU FICHIER HTML DE LA PAGE WEB
+const char * nomDeFichier = "/index.html";  // <<--- NOM DU FICHIER HTML DE LA PAGE WEB
+const char* ssid = "SSID";                  // <<--- METTRE ICI VOTRE NOM RESEAU WIFI
+const char* password = "MDP";               // <<--- METTRE ICI VOTRE MOT DE PASSE WIFI
 
-const char* ssid = "SSID";          // <<--- METTRE ICI VOTRE NOM RESEAU WIFI
-const char* password = "MDP";       // <<--- METTRE ICI VOTRE MOT DE PASSE WIFI
+const byte pinLed = D1;
 
-//Ouverture du port http pour la page web
+// Initialisation des Pin Entrés et Sortie de l'ESP
+void InitPin() {
+  pinMode(pinLed, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+// Crée un serveur sur le port HTTP standard (80)
 const uint16_t HTTPPort = 80;
 WiFiServer serveurWeb(HTTPPort);
 
+const byte maxHTTPLine = 100;
+char httpLine[maxHTTPLine + 1];          // +1 pour avoir la place du '\0'
 
-//Récupération et inscription dans le terminale, l'ip de l'ESP
-void printHTTPServerInfo() {
+const byte maxURL = 50;
+char urlRequest[maxURL + 1];             // +1 pour avoir la place du '\0'
 
-  Serial.print(F("Site web http://")); Serial.print(WiFi.localIP());
-  if (HTTPPort != 80) {
-    
-    Serial.print(F(":"));
-    Serial.print(HTTPPort);
-  }
-  Serial.println();
+// Initialisation du WiFi + serveur Web
+void InitWiFi() {
 
-}
-
-//Génération de la page web grace au fichier html dans la mémoire flash
-void testRequeteWeb() {
-
-  boolean currentLineIsBlank = true;
-
-  WiFiClient client = serveurWeb.available();
-  if (!client) return; // pas de client connecté
-
-  while (client.connected()) {
-    if (client.available()) {
-      // on lit toute la trame HTPP, ici sans se soucier de la reqête
-      char c = client.read();
-
-      if (c == '\n' && currentLineIsBlank) { // une requête HTTP se termine par une ligne vide
-        // ON GENERE LA PAGE WEB
-        // On envoie un en tête de réponse HTTP standard
-        client.println(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"));
-        
-        if (LittleFS.exists(nomDeFichier)) {
-          File pageWeb = LittleFS.open(nomDeFichier, "r");
-          client.write(pageWeb);
-          pageWeb.close();
-        } else {
-          Serial.println(F("Erreur de fichier"));
-        }
-        // on sort du while et termine la requête
-        break;
-      }
-      if (c == '\n') currentLineIsBlank = true;
-      else if (c != '\r') currentLineIsBlank = false;
-    } // end if available
-  } // end while
-  delay(1);
-  client.stop(); // termine la connexion
-
-}
-
-
-void setup() {
-  Serial.begin(9600); //ouverture communication série a 9600bit/s
-  Serial.println("\n\nTest SPIFFS\n");
-
-  // on démarre le SPIFSS
-  if (!LittleFS.begin()) {
-    Serial.println("erreur SPIFFS");
-    while (true); // on ne va pas plus loin
-  }
-
-  //connection au WiFi
+  // Connexion au Réseaux WiFi
   WiFi.begin(ssid, password);
-
-  //donne le statue de connexion au WiFi
   Serial.println();
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -87,13 +41,137 @@ void setup() {
   }
   Serial.println();
 
-  // on démarre le serveur
+  // Démarrage Serveur Web
   serveurWeb.begin();
-  printHTTPServerInfo();
+
+  // Donne l'ip de Connexion au réseaux dans le Terminal
+  Serial.print(F("Site web http://")); Serial.print(WiFi.localIP());
+  if (HTTPPort != 80) {
+    Serial.print(F(":"));
+    Serial.print(HTTPPort);
+  }
+  Serial.println();
+}
+
+//démarrage de LittleFS (Gestionnaire de fichier) pour la page HTML
+void InitLittleFS() {
+  Serial.println("\n\nTest LittleFS\n");
+
+  // Si il y a une erreur, on ne vas pas plus loins
+  if (!LittleFS.begin()) {
+    Serial.println("erreur LittleFS");
+    while (true);
+  }
+}
+
+// Fonction RelayRhum(cl) est utiliser pour allumer
+// le Relay lors d'un appuis sur le bouton de la page WEB
+// ou grace a l'application
+void RelayRhum(WiFiClient &cl) {
+
+  digitalWrite(pinLed, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
+  cl.println("En cours");
+  delay(2000);
+  digitalWrite(pinLed, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);
+  cl.println("Terminer");
 
 }
 
-//Démarrage de la fonction testRequeteWeb() pour la création de la page Web
+// Gestion des requette HTTP
+boolean testRequeteWeb() {
+  boolean requeteHTTPRecue = false;
+  byte indexMessage = 0;
+  char * ptrGET, *ptrEspace;
+
+  WiFiClient client = serveurWeb.available();
+
+  //on vérifie si des client son connecter au WiFi
+  if (!client) return requeteHTTPRecue;
+
+  boolean currentLineIsBlank = true;
+  while (client.connected()) {
+    if (client.available()) {
+      char c = client.read(); 
+
+      // une requête HTTP se termine par une ligne vide
+      if (c == '\n' && currentLineIsBlank) {
+
+        // Ceci n'est pas pour le favicon du site Web
+        if (strcmp(urlRequest, "/favicon.ico")) {
+          requeteHTTPRecue = true;
+
+          // On envoie un en tête de réponse HTTP standard
+          client.println("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: keep-alive\r\n");
+
+          // On regarde si on a une requete qui continet "/reqEtatBouton"
+          // si c'est le cas, on appelle la fonction RelayRhum() qui allume le relay
+          if (strstr(urlRequest, "/reqEtatBouton")) {
+            RelayRhum(client);
+          }
+
+          // On regarde si on a une requete qui continet "/connexion"
+          // si c'est le cas, elle permet la validation de l'adresse ip sur l'application
+          if (strstr(urlRequest, "/connexion")) {
+            client.println("connexion OK");
+          }
+
+          /* Affichage de la page Web (pas utiliser pour le moment)
+          
+            if (LittleFS.exists(nomDeFichier)) {
+              File pageWeb = LittleFS.open(nomDeFichier, "r");
+              client.write(pageWeb);
+              pageWeb.close();
+            } else {
+              Serial.println(F("Erreur de fichier"));
+            }
+
+          */
+            
+        }
+        break;
+      } // fin de génération de la réponse HTTP
+
+      if (c == '\n') {
+        currentLineIsBlank = true;
+        httpLine[indexMessage] = '\0';             // on termine la ligne correctement (c-string)
+        indexMessage = 0;                          // on se reprépre pour la prochaine ligne
+        if (ptrGET = strstr(httpLine, "GET")) {
+          // c'est la requête GET, la ligne continent "GET /URL HTTP/1.1", on extrait l'URL
+          ptrEspace = strstr(ptrGET + 4, " ");
+          *ptrEspace = '\0';
+          strncpy(urlRequest, ptrGET + 4, maxURL);
+          urlRequest[maxURL] = '\0';               // par précaution si URL trop longue
+        }
+      } else if (c != '\r') {
+        currentLineIsBlank = false;
+        if (indexMessage <= maxHTTPLine - 1) {
+          httpLine[indexMessage++] =  c;           // sinon on ignore le reste de la ligne
+        }
+      }
+    }   // end if available
+  }     // end while
+  delay(1);
+  client.stop(); // termine la connexion
+  return requeteHTTPRecue;
+}
+
+void setup() {
+  // Vitesse de la communication Série 9600 Bit/s
+  Serial.begin(9600);
+
+  // Initialisation des Pin Entrés est Sortie de l'ESP
+  InitPin();
+
+  //démarrage de LittleFS (Gestionnaire de fichier) pour la page HTML
+  InitLittleFS();
+
+  // Apelle de la fonction InitWiFi() pour démaré la 
+  // Comunication Entre le Wifi est le Programme
+  InitWiFi();
+}
+
 void loop() {
   testRequeteWeb();
 }
