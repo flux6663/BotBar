98cd096bca98fbece59e5f64f26d79b11dc91e7d
MARTIN Mathis <flux6593@gmail.com>
2023-05-21 10:57:41 +0200

    Création d'un Wifi "botbar" Création d'un réseau local "192.168.1.1/24" mise en place d'une résolution DNS "botbar.fr"
    création des Boisson :
    création de l'api "http://botbar.fr/api/mix"
    mettre en paramétre : "drink" puis le nom de la boisson a créé
    
    service automatique de la boisson :
    les relais s'allume et s'éteigne le temp qu'il faut pour créé la boisson avec les bonne proportion
    
    en meme temp,
    l'anneau lumineaux crée un effect arc en ciel :
    - 100 % tu temp de restant = tout les led alumer
    - 50 % la moiter des led son allumer
    - 0 % tout les led son eteint

--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,211 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <WiFi.h>
 #include <DNSServer.h>
-#include <WiFiManager.h>
-#include <LittleFS.h>
+#include <ESPAsyncWebServer.h>
+#include <Adafruit_NeoPixel.h>
+#include <vector>
 
-const char * nomDeFichier = "/Hello.html";
+#define DNS_PORT 53
+#define NUM_DRINKS 16
+#define NUM_INGREDIENTS 8
 
-const char* ssid = "NOM RESEAU WIFI";          // <<--- METTRE ICI VOTRE NOM RESEAU WIFI
-const char* password = "MOT DE PASSE WIFI";    // <<--- METTRE ICI VOTRE MOT DE PASSE WIFI
+#define NUM_LEDS 24       // Le nombre de LEDs sur votre bande
+#define PIN 4             // Le pin sur lequel votre bande est connectée
 
-const uint16_t HTTPPort = 80;
-WiFiServer serveurWeb(HTTPPort); // crée un serveur sur le port HTTP standard
 
-void printHTTPServerInfo()
-{
-  Serial.print(F("Site web http://")); Serial.print(WiFi.localIP());
-  if (HTTPPort != 80) {
-    
-    Serial.print(F(":"));
-    Serial.print(HTTPPort);
+IPAddress apIP(192, 168, 1, 1);
+DNSServer dnsServer;
+AsyncWebServer server(80);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+int8_t Demande_Boisson;
+bool boisson = false;
+
+//Define your relay GPIOs
+const int relay[NUM_INGREDIENTS] = {32, 33, 25, 26, 27, 14, 23, 22};
+
+struct Ingredient {
+    int pump;
+    long duration;
+};
+
+struct Drink {
+    String name;
+    std::vector<Ingredient> ingredients;
+    unsigned long totalDuration;  // Ajouté
+    unsigned long drinkStartTime;  // Ajouté
+};
+
+struct ActiveDrink {
+  Drink drink;
+  int currentIngredientIndex;
+  unsigned long startTime;
+};
+
+// Définir les ingrédients pour chaque boisson
+std::vector<Ingredient> ingredients_menthe_a_l_eau = {{27, 12000}, {32, 48000}};
+std::vector<Ingredient> ingredients_grenadine_a_l_eau = {{14, 12000}, {32, 48000}};
+std::vector<Ingredient> ingredients_diabolo_grenadine = {{23, 48000}, {14, 12000}};
+std::vector<Ingredient> ingredients_diabolo_menthe = {{23, 48000}, {27, 12000}};
+std::vector<Ingredient> ingredients_jus_de_fruit = {{32, 36000}, {33, 36000}, {25, 24000}, {26, 12000}};
+std::vector<Ingredient> ingredients_special_botbar = {{32, 36000}, {33, 36000}, {25, 24000}, {26, 12000}, {27, 12000}, {14, 12000}, {23, 24000}, {22, 12000}};
+std::vector<Ingredient> ingredients_afterglow = {{33, 36000}, {14, 12000}, {22, 12000}};
+std::vector<Ingredient> ingredients_lemon = {{25, 24000}, {23, 48000}};
+std::vector<Ingredient> ingredients_danies = {{32, 36000}, {25, 24000}, {23, 48000}};
+std::vector<Ingredient> ingredients_borabora = {{33, 36000}, {25, 24000}, {26, 12000}, {23, 48000}};
+std::vector<Ingredient> ingredients_bella_luna = {{32, 36000}, {33, 36000}, {23, 48000}};
+std::vector<Ingredient> ingredients_mojito = {{25, 24000}, {27, 12000}, {23, 48000}};
+std::vector<Ingredient> ingredients_splendide_beaute = {{32, 36000}, {14, 12000}, {23, 48000}};
+std::vector<Ingredient> ingredients_letitia_special = {{33, 36000}, {27, 12000}, {22, 12000}};
+std::vector<Ingredient> ingredients_sex_on_the_beach = {{32, 36000}, {33, 36000}, {14, 12000}, {23, 48000}};
+std::vector<Ingredient> ingredients_pina_colada = {{33, 36000}, {22, 12000}};
+
+// Définir vos boissons ici
+std::vector<Drink> drinks = {
+    { "Menthe à l'eau", ingredients_menthe_a_l_eau },
+    { "Grenadine à l'eau", ingredients_grenadine_a_l_eau },
+    { "Diabolo Grenadine", ingredients_diabolo_grenadine },
+    { "Diabolo Menthe", ingredients_diabolo_menthe },
+    { "100% Jus de fruit", ingredients_jus_de_fruit },
+    { "Spécial BotBar", ingredients_special_botbar },
+    { "Afterglow", ingredients_afterglow },
+    { "Lemon", ingredients_lemon },
+    { "Danies", ingredients_danies },
+    { "BoraBora", ingredients_borabora },
+    { "Bella Luna", ingredients_bella_luna },
+    { "Mojito", ingredients_mojito },
+    { "Splendide Beaute", ingredients_splendide_beaute },
+    { "Letitia Special", ingredients_letitia_special },
+    { "Sex on the beach", ingredients_sex_on_the_beach },
+    { "Piña Colada", ingredients_pina_colada }
+};
+
+ActiveDrink* activeDrink = nullptr;
+
+void startMixing(Drink drink) {
+  // Calculer la durée totale de la boisson
+  unsigned long totalDuration = 0;
+  for (const auto& ingredient : drink.ingredients) {
+      totalDuration += ingredient.duration;
   }
-  Serial.println();
+  drink.totalDuration = totalDuration;
+
+  // Enregistrer le moment où nous avons commencé à préparer cette boisson
+  drink.drinkStartTime = millis();
+
+  activeDrink = new ActiveDrink { drink, 0, millis() };
+  digitalWrite(drink.ingredients[0].pump, LOW);
 }
 
-void testRequeteWeb()
-{
-  boolean currentLineIsBlank = true;
-
-  WiFiClient client = serveurWeb.available();
-  if (!client) return; // pas de client connecté
-
-  while (client.connected()) {
-    if (client.available()) {
-      // on lit toute la trame HTPP, ici sans se soucier de la reqête
-      char c = client.read();
-
-      if (c == '\n' && currentLineIsBlank) { // une requête HTTP se termine par une ligne vide
-        // ON GENERE LA PAGE WEB
-        // On envoie un en tête de réponse HTTP standard
-        client.println(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"));
-        
-        if (LittleFS.exists(nomDeFichier)) {
-          File pageWeb = LittleFS.open(nomDeFichier, "r");
-          client.write(pageWeb);
-          pageWeb.close();
-        } else {
-          Serial.println(F("Erreur de fichier"));
+void handleRequest(AsyncWebServerRequest *request) {
+    if (request->hasParam("drink")) {
+        String drinkName = request->getParam("drink")->value();
+        auto it = std::find_if(drinks.begin(), drinks.end(), [&drinkName](const Drink& drink) {
+            return drink.name == drinkName;
+        });
+        if (it != drinks.end()) {
+            request->send(200, "text/plain", "Mélange de " + drinkName);
+            Serial.println("Mélange de " + drinkName);
+            if (activeDrink != nullptr) {
+            delete activeDrink;
+            }
+            startMixing(*it);
+            return;
         }
-        // on sort du while et termine la requête
-        break;
-      }
-      if (c == '\n') currentLineIsBlank = true;
-      else if (c != '\r') currentLineIsBlank = false;
-    } // end if available
-  } // end while
-  delay(1);
-  client.stop(); // termine la connexion
+        request->send(404, "text/plain", "Boisson introuvable");
+        Serial.println("Boisson introuvable");
+    } else {
+        request->send(400, "text/plain", "Paramètre 'drink' manquant");
+        Serial.println("Paramètre 'drink' manquant");
+    }
 }
 
+uint32_t Wheel(byte WheelPos) {
+    WheelPos = 255 - WheelPos;
+    if(WheelPos < 85) {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    }
+    if(WheelPos < 170) {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+    WheelPos -= 170;
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+// Cette fonction crée un effet d'arc-en-ciel
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
 
 void setup() {
-  Serial.begin(9600); // parce que mon Wemos et par défaut à peu près à cette vitesse, évite les caractères bizarre au boot
-  Serial.println("\n\nTest SPIFFS\n");
+    //Setup Relay GPIOs
+    for (int i=0; i<8; i++) {
+        pinMode(relay[i], OUTPUT);
+        digitalWrite(relay[i], HIGH);
+    }
+    Serial.begin(115200);
 
-  // on démarre le SPIFSS
-  if (!LittleFS.begin()) {
-    Serial.println("erreur SPIFFS");
-    while (true); // on ne va pas plus loin
-  }
+    // Start WiFi
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+    WiFi.softAP("BotBar");
 
-  WiFi.begin(ssid, password);
+    // if DNSServer is started with "*" for domain name, it will reply with
+    // provided IP to all DNS request
+    dnsServer.start(DNS_PORT, "botbar.fr", apIP);
 
-  Serial.println();
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.write('.');
-  }
-  Serial.println();
+    // Gestionnaire d'URL pour la requête POST
+    server.on("/api/mix", HTTP_POST, handleRequest);
 
-  // on démarre le serveur
-  serveurWeb.begin();
-  printHTTPServerInfo();
+    server.begin();
 
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-  testRequeteWeb();
+    dnsServer.processNextRequest();
+
+    if (activeDrink != nullptr) {
+        Ingredient& currentIngredient = activeDrink->drink.ingredients[activeDrink->currentIngredientIndex];
+        if (millis() - activeDrink->startTime >= currentIngredient.duration) {
+            digitalWrite(currentIngredient.pump, HIGH);
+            activeDrink->currentIngredientIndex++;
+            if (activeDrink->currentIngredientIndex >= activeDrink->drink.ingredients.size()) {
+                delete activeDrink;
+                activeDrink = nullptr;
+            } else {
+                currentIngredient = activeDrink->drink.ingredients[activeDrink->currentIngredientIndex];
+                digitalWrite(currentIngredient.pump, LOW);
+                activeDrink->startTime = millis();
+            }
+
+            
+        }
+
+        // Gestion des LEDs seulement si activeDrink n'est pas nullptr
+        if (activeDrink != nullptr) {
+            unsigned long elapsed = millis() - activeDrink->drink.drinkStartTime;
+            unsigned long totalDuration = activeDrink->drink.totalDuration;
+            int numActiveLEDs = NUM_LEDS * (totalDuration - elapsed) / totalDuration;
+            for (int i = 0; i < numActiveLEDs; i++) {
+                strip.setPixelColor(i, Wheel((i + millis() / 100) % 256)); // Scroll through the color wheel
+            }
+            for (int i = numActiveLEDs; i < NUM_LEDS; i++) {
+                strip.setPixelColor(i, 0); // Turn off the remaining LEDs
+            }
+            strip.show();
+        }
+    }
+
+
 }
-
-
-
-//j'ai rajouter des commantaire ...
